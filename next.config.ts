6dc6cd66534230f9c2645c149814e252e450755c
Doseung Yang <doseung.dev@gmail.com>
2025-09-16 18:09:48 +0900

    feat: notion dependency 설치

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,28 @@ const nextConfig = {
   experimental: {},
   images: {
     formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+      },
+    ],
   },
   compress: true,
   reactStrictMode: true,
